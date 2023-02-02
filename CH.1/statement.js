dece07aa1213c79e3883300583f9310c03331f4a
pooreumu <pooreum83@gmail.com>
2023-02-02 20:25:40 +0900

    ♻️ volumeCredits 변수 제거하기: 문장 슬라이드하기

--- CH.1/statement.js
+++ CH.1/statement.js
@@ -1,6 +1,5 @@
 module.exports = function statement(invoice, plays) {
     let totalAmount = 0
-    let volumeCredits = 0
     let result = `청구내역 (고객명: ${invoice.customer})\n`
 
     for (let perf of invoice.performances) {
@@ -11,6 +10,7 @@ module.exports = function statement(invoice, plays) {
         totalAmount += amountFor(perf)
     }
 
+    let volumeCredits = 0
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf)
     }
