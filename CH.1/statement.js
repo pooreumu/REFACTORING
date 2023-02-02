2c66c7b01917974f21b9d6ac959bd7eddcfef4b3
pooreumu <pooreum83@gmail.com>
2023-02-02 20:24:42 +0900

    ♻️ volumeCredits 변수 제거하기: 반복문 쪼개기

--- CH.1/statement.js
+++ CH.1/statement.js
@@ -4,13 +4,15 @@ module.exports = function statement(invoice, plays) {
     let result = `청구내역 (고객명: ${invoice.customer})\n`
 
     for (let perf of invoice.performances) {
-        // 포인트를 적립한다.
-        volumeCredits += volumeCreditsFor(perf)
-
+        // 청구 내역을 출력한다.
         result += `${playFor(perf).name} : ${usd(amountFor(perf))} (${
             perf.audience
         }석)\n`
         totalAmount += amountFor(perf)
+    }
+
+    for (let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf)
     }
 
     result += `총액: ${usd(totalAmount)}\n`
